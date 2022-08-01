a39d25e8583d175cfb6ecce7172f02730921b3ec
Lingxin Ma <66456375+AtticusAlan@users.noreply.github.com>
2022-08-01 15:13:44 -0400

    set up backend structure

---
+++ index.js
@@ -0,0 +1,10 @@
+import app from './server.js';
+import mongodb from "mongodb";
+import dotenv from "dotenv";
+import ActivsDAO from './dao/activsDAO.js';
+import ReviewsDAO from './dao/reviewsDAO.js';
+import FavoritesDAO from './dao/favoritesDAO.js';
+
+async function main() {
+  
+}
